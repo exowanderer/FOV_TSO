3641582a8ccd70c83b852492e5fa6dbe489754f3
Jonathan Fraine <exowanderer@users.noreply.github.com>
2017-10-04 19:44:18 -0400

    Changed defaults to be smaller

--- generate_FOVs.py
+++ generate_FOVs.py
@@ -25,8 +25,8 @@ def stellarModels(times, nTimes, nStars, minLogPeriod, maxLogPeriod, nPeriods, s
   
   return starModels
 
-def generate_field_of_view(nTimes=1000, imageSize=1024, nStars=100, 
-                           fwhm=3., tMin=0, tMax=10, nPeriods=10,
+def generate_field_of_view(nTimes=100, imageSize=100, nStars=10, 
+                           fwhm=3., tMin=0, tMax=10, nPeriods=5,
                            minLogPeriod=-3, maxLogPeriod=2,
                            stdAmp=5e-3, returnAll=False):
   
