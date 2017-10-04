e171f7a5356688905f3efbb298082e15b1808636
Jonathan Fraine <exowanderer@users.noreply.github.com>
2017-10-04 17:14:12 -0400

    Rough Draft of FFI code
    For ground based TSO data, we can extract the a light curve for every star in the field. Then we'll use that to match up common mode variations.
    
    This should map out the atmospheric fluctuations and the detector spatial variations (i.e. flat field and vignetting)

---
+++ FOV_TSO.py
@@ -0,0 +1,58 @@
+from photutils import CircularAperture, Gaussian2D, Const2D
+from photutils import DAOStarFinder, find_peaks
+
+from astropy.modeling import models, fitting
+from astropy.stats import sigma_clipped_stats
+
+mean, median, std = sigma_clipped_stats(data, sigma=3.0, iters=5)
+
+nFWHM         = 2.0
+nPixelsSeeing = 3.0
+nSig          = 5.0
+daofind = DAOStarFinder(fwhm=nPixelsSeeing, threshold=nSig*std)
+
+useDAO = False:
+if useDAO:
+  sources = daofind(data - median)
+else:
+  sources = find_peaks(data, threshold, box_size=5)
+
+aperRad = 1.5*nPixelsSeeing
+
+psSize        = nFWHM*nPixelSeeing
+postageStamps = []
+for source in sources:
+  yc0,xc0 = source['ycentroid'], source['xcentroid']
+  postageStamps.append(data[yc0-psSize:yc0+psSize, xc0-psSize, xc0+psSize])
+
+amplitude = 1.0
+offset    = median
+y_mean    = psSize
+x_mean    = psSize
+y_stddev  = nPixelsSeeing
+x_stddev  = nPixelsSeeing
+theta     = 0.0
+
+yy, xx    = np.indices((psSize, psSize)) - 0.5*psSize # center at zero for later
+initModel = Gaussian2D(amplitude=amplitude, x_mean=x_mean, y_mean=y_mean, x_stddev=x_sstddev, y_stddev=y_stddev, theta=theta)
+initModel = initModel + Const2D(amplitude=offset)
+
+fit_lvmq  = fitting.LevMarLSQFitter()
+
+positions = []
+gaussFits = []
+for image in images:
+  gaussFits.append([])
+  positions.append([])
+  for postageStamp in postageStamps:
+    nowModel = initModel
+    nowModel.y_mean = yc
+    nowModel.x_mean = xc
+
+    gaussFits[-1].append(fit_lvmq(nowModel, xx+xc,yy+yc, postageStamp))
+  
+  positions = [gfit['xcentroid'], gfit['ycentroid'] for gfits in gaussFits]
+
+  apertures = CircularAperture(positions, r=aperRad)
+  
+  phot_table = aperture_photometry(data, apertures)
